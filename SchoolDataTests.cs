a77dccdf48f9efab6382f48b9454ff9443a75de2
priyanka <priyankatripathy81@gmail.com>
2024-02-23 20:41:09 +0530

    adding all the files

---
+++ SchoolDataTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Moq;
+using SchoolDataLibrary;
+
+namespace SchoolDataLibrary
+{
+    [TestFixture]
+    public class SchoolDataTests
+    {
+        [Test]
+        public void TestTeacherSalary()
+        {
+            // Arrange
+            var teacher = new Teacher();
+            teacher.Salary = 50000;
+
+            // Act
+            var actualSalary = teacher.Salary;
+
+            // Assert
+            Assert.AreEqual(50000, actualSalary);
+        }
+
+        // Add more test methods for other classes and scenarios...
+
+        [Test]
+        public void TestSubjectDependency()
+        {
+            // Arrange
+            var mockTeacher = new Mock<Teacher>();
+            mockTeacher.Setup(t => t.GetSubject()).Returns("Math");
+            var subject = new Subject(mockTeacher.Object);
+
+            // Act
+            var subjectTeacher = subject.GetTeacher();
+
+            // Assert
+            Assert.AreEqual("Math", subjectTeacher.GetSubject());
+        }
+    }
+}
+
+
---
+++ SchoolRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDataLibrary
+{
+    public class SchoolRepository
+    {
+        private List<Teacher> teachers = new List<Teacher>();
+        private List<Student> students = new List<Student>();
+        private List<Subject> subjects = new List<Subject>();
+
+        // Methods for managing teachers
+        public void AddTeacher(Teacher teacher)
+        {
+            teachers.Add(teacher);
+        }
+
+        public Teacher GetTeacherById(int id)
+        {
+            return teachers.Find(t => t.Id == id);
+        }
+
+        public object GetTeachers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
